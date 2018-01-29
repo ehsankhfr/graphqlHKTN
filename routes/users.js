8567ef1421ffac3dc44faf9d438aac37d700cf45
ehsankhfr <ehsankhfr@gmail.com>
2018-01-29 12:12:44 +0000

    App code is added for a sample schema

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+	hello: () => {
+		return 'Hello world!';
+	},
+};
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.use('/gql', graphqlHTTP({
+		schema: schema,
+		rootValue: root,
+		graphiql: true,
+	})
+);
 
 module.exports = router;
